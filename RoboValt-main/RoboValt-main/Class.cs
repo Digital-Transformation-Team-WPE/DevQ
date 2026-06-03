a93e5165c902ce539b7dfce2de013edf8ae8c04f
JagatheeshK <jagatheesh.k@external.stellantis.com>
2026-06-03 19:18:51 +0530

    secondcommit: Update appsettings.json with new Azure AD client secret and enable Azure AD authentication. Also, remove unused state variables related to Azure AD users in Projects.jsx

--- RoboValt-main/RoboValt-main/Class.cs
+++ RoboValt-main/RoboValt-main/Class.cs
@@ -2,9 +2,9 @@
 {
     public class EmployeeInformation
     {
-        private const string TenantId = "d852d5cd-724c-4128-8812-ffa5db3f8507";
-        private const string ClientId = "1e925e53-50de-41de-8b02-55b0c050e092";
-        private const string ClientSecret = "-ix8Q~o0tpxfQfxaVlgfdva5m8zDv1Tmv.WjicjP";
+        private const string TenantId = "";
+        private const string ClientId = "";
+        private const string ClientSecret = "";
 
 
     }
