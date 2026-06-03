205b9ad5ba891f188cd8dc09e1b2c994d639fac2
JagatheeshK <jagatheesh.k@external.stellantis.com>
2026-06-03 19:25:23 +0530

    removedClientSecrets

--- RoboValt-main/RoboValt-main/Class.cs
+++ RoboValt-main/RoboValt-main/Class.cs
@@ -2,9 +2,9 @@
 {
     public class EmployeeInformation
     {
-        private const string TenantId = "";
-        private const string ClientId = "";
-        private const string ClientSecret = "";
+        private const string TenantId = "<REPLACE_IN_AZURE>";
+        private const string ClientId = "<REPLACE_IN_AZURE>";
+        private const string ClientSecret = "<REPLACE_IN_AZURE>";
 
 
     }
